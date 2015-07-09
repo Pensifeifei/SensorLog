c6748279ed02aabd57e9fef20175cc9e9f127710
Mr.Chang <mrchang@MrChangdeMac-mini.local>
2015-07-09 10:39:59 +0800

    Initial Commit

---
+++ ACC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ACC
+//
+//  Created by Mr.Chang on 15/7/9.
+//  Copyright (c) 2015年 Mr.Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ACC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ACC
+//
+//  Created by Mr.Chang on 15/7/9.
+//  Copyright (c) 2015年 Mr.Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
